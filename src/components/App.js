6f197b3eec856c44057a9a54348bf108165234d1
Mariko Wakabayashi <mwakabayashi@groupon.com>
2017-04-17 12:45:04 -0500

    update order state

--- src/components/App.js
+++ src/components/App.js
@@ -11,6 +11,7 @@ class App extends React.Component {
     // initial State
     this.addFish = this.addFish.bind(this);
     this.loadSamples = this.loadSamples.bind(this);
+    this.addToOrder = this.addToOrder.bind(this);
     this.state = {
       fishes: {},
       order: {}
@@ -30,6 +31,15 @@ class App extends React.Component {
     });
   }
 
+  addToOrder(key) {
+    // take a copy of our state
+    const order = {...this.state.order};
+    // update or add the new number of fish ordered
+    order[key] = order[key] + 1 || 1;
+    // update our state
+    this.setState({ order });
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -39,7 +49,12 @@ class App extends React.Component {
             {
               Object
                 .keys(this.state.fishes)
-                .map(key => <Fish key={key} details={this.state.fishes[key]} />)
+                .map(key => <Fish
+                              key={key}
+                              index={key}
+                              details={this.state.fishes[key]}
+                              addToOrder={this.addToOrder} />
+                )
             }
           </ul>
         </div>
