f0d436fa658a0f272fe70ac7db8e4590fedde3e7
Mariko Wakabayashi <mwakabayashi@groupon.com>
2017-04-24 12:58:26 -0500

    persist data with firebase

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyDIH-FgfDrqlIx2CIy-_I7laSqV9KqdD2s",
+  authDomain: "catch-of-the-day-mariko.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-mariko.firebaseio.com",
+});
+
+export default base;
